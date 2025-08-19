d5b57d6ac57c983b709db917ed1e3c1193d4d04e
Mr.Awesome25 <wkpadgett4750@gmail.com>
2025-08-18 22:22:08 -0500

    Create HelloWorld.java
    created first ever java program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class HelloWorld extends OpMode {
+    @Override
+    public void init(){
+        telemetry.addData("Hello", "World");
+
+    }
+    @Override
+    public void loop(){
+
+    }
+}
